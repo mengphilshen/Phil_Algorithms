57ef0a1d163e4312f724c3a93c27383798c36100
phil <caassm@ufl.edu>
2019-05-29 23:38:53 -0400

    LeetCode_7_Reverse_Integer

---
+++ LeetCode/[007_Java]Reverse_Integer.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    /**
+     * Given a 32-bit signed integer, reverse digits of an integer
+     * @param x an integer
+     * @return reverse digits of an integer
+     */
+
+    // Pop and Push Digits
+    public int reverseInteger(int x) {
+        long rev = 0;
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10; 
+        }
+        if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE)
+            return (int)rev;
+        else
+            return 0;
+    }
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+ 
+}
+
+
